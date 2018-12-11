87f938a07206402bce76bcdc1850e989520db8e0
YannLocatelli <45776332+YannLocatelli@users.noreply.github.com>
2018-12-11 15:19:18 +0800

    Update Neural network.py
    Test to commit from Github app

--- Neural network.py
+++ Neural network.py
@@ -1,10 +1,11 @@
-import matplotlib.pyplot as plt
+#Code from ytchen2010
+
+
 import numpy as np
 
 rng = np.random.RandomState(42)
 x = 10 * rng.rand(50)
 y = 2 * x - 1 + rng.randn(50)
-plt.scatter(x, y);
 
 from sklearn.linear_model import LinearRegression
 
@@ -20,7 +21,5 @@ model.intercept_
 xfit = np.linspace(-1, 11)
 Xfit = xfit[:, np.newaxis]
 yfit = model.predict(Xfit)
-plt.scatter(x, y)
-plt.plot(xfit, yfit);
 
 #fit random data set
