ada31a91602cae07d0a1a0d3ae3a00f2390248d9
YannLocatelli <45776332+YannLocatelli@users.noreply.github.com>
2018-12-12 16:35:30 +0800

    Update Neural network.py
    Move Neural Network code to Neural network.py file

--- Neural network.py
+++ Neural network.py
@@ -1,25 +1,14 @@
-#Code from ytchen2010
+from ReadCSV import *
+(X,y) = convert_CsvToData('TraData.csv')
+(Xt, yt) = convert_CsvToData('input.csv',TrainingData = False)
 
+#Neural Networks library
+import sklearn.neural_network as nn
 
-import numpy as np
+#Number of layers
+n = 10
+model = nn.MLPClassifier(n)
+model.fit(X,y)
 
-rng = np.random.RandomState(42)
-x = 10 * rng.rand(50)
-y = 2 * x - 1 + rng.randn(50)
-
-from sklearn.linear_model import LinearRegression
-
-model = LinearRegression(fit_intercept=True)
-model
-
-X = x[:, np.newaxis]
-X.shape
-
-model.fit(X, y)
-model.coef_
-model.intercept_
-xfit = np.linspace(-1, 11)
-Xfit = xfit[:, np.newaxis]
-yfit = model.predict(Xfit)
-
-#fit random data set
+yt = model.predict(Xt)
+convert_DataToCsv(yt,'output.csv')
