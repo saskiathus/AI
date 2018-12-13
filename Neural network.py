0ef82c6e5e26717be76cd6ae07f442cfa92ac0ee
YannLocatelli <45776332+YannLocatelli@users.noreply.github.com>
2018-12-14 03:37:27 +0800

    Update Neural network.py
    Function Results added, F-Measure included.
    All libraries at the same place
    Score <=> Accuracy

--- Neural network.py
+++ Neural network.py
@@ -1,11 +1,25 @@
-from ReadCSV_V2 import *
+from ReadCSV import *
+import pickle
+import sklearn.neural_network as nn
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+import random as rd
+import numpy as np
+
+def Results(brain, y_true, y_pred):
+    #pickle.dump(brain, open("brain", "wb"))
+    acc = accuracy_score(y_true, y_pred)
+    prec = precision_score(y_true, y_pred)
+    rec = recall_score(y_true, y_pred)
+    F = 2 * prec * rec / (prec + rec)
+    print "Accuracy:",acc,"\nPrecision:",prec,"\nRecall:",rec,"\nF-Measure:",F,"\n"
+    
+
 #X, y = convert_CsvToData('TraData.csv')
 X, y = convert_CsvToData_ratio('TraData.csv')
 Xt, yt = convert_CsvToData('input.csv',TrainingData = False)
 print "Data extraction completed\n"
-
-#Neural Networks library
-import sklearn.neural_network as nn
 
 #Number of layers
 n = 100
@@ -13,9 +27,6 @@ model = nn.MLPClassifier(hidden_layer_sizes = n, activation = 'logistic',
                          solver = 'lbfgs',random_state = None)
 
 #k-fold model
-import random as rd
-import numpy as np
-
 for i in range(1000):
     k = rd.randint(5,20)
     k_size = float(X.shape[0])/k
@@ -23,6 +34,8 @@ for i in range(1000):
         sampleBot = int(k_size * j)
         sampleTop = int(k_size * (j+1))
         model.fit(X[sampleBot:sampleTop],y[sampleBot:sampleTop])
-    print i+1,". Score:",model.score(X[sampleTop:],y[sampleTop:])
+    print i+1,"."
+    Results(model,y[sampleTop:],model.predict(X[sampleTop:]))
     yt = model.predict(Xt)
     convert_DataToCsv(yt,'output.csv')
+
