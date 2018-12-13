357861e65d70b6beeda32b01dd7a83e7b7676190
YannLocatelli <45776332+YannLocatelli@users.noreply.github.com>
2018-12-14 02:56:09 +0800

    Update ReadCSV.py
    - Comments added.
    - 'ip' column removed
    - [In convert_CstToData_ratio]positive and negative value become p and n rather than r-tuple. data become rawData and X become data
    
    Utilization of an encoder rather than transforming string in float. convert_string was removed.
    Now matrix have a larger size, depending on the encoder.

--- ReadCSV.py
+++ ReadCSV.py
@@ -2,63 +2,110 @@
 
 #Use pandas to avoid the commas issue
 import pandas as pd
+import numpy as np
 import random as rd
+import os
+import pickle
 from sklearn.utils import shuffle
+from sklearn.preprocessing import OneHotEncoder
 
 def convert_CsvToData(name, TrainingData = True):
+    """Extract all data from a csv file. Each column is delemited by a comma.
+
+    Parameters
+    ----------
+    name: Name or relative path of the file with the extension.
+
+    TrainingData: True if the file contain Target values.
+        Otherwise it consider to contain only Inputs to make prediction.
+
+    Returns
+    -------
+    inData, outData: tuple containing a DataFrame of Inputs of the datafile and
+        an array of Target. The array of Target is full of 0 if TrainingData is set False.
+    """
     #Extract data from csv file, and put Nan value to 0
     rawData = pd.read_csv(name, delimiter = ',', dtype={"dclkVerticals": object})
+    #del rawData['ip']
+    rawData.drop('ip', axis=1, inplace=True)
     rawData = rawData.fillna(0)
 
-    #Neural Networks doesn't support string, so convert them
-    for i in range(10):
-        rawData[rawData.columns[i]] = rawData[rawData.columns[i]].apply(convert_string)
-
-    #Seperate inputs and outputs
-    if TrainingData:    
-        inData = rawData.iloc[:,:-1]
+    #Neural Networks doesn't support string, so convert them => Encoder
+    if TrainingData:
+        if not(os.path.isfile('enc')):
+            enc = OneHotEncoder(handle_unknown='ignore')
+            enc.fit(rawData.iloc[:,:-1])
+            pickle.dump(enc, open("enc", "wb"))
+        else:
+            enc = pickle.load(open("enc","rb"))
+        inData = pd.DataFrame(enc.transform(rawData.iloc[:,:-1]).toarray())
         outData = rawData.iloc[:,-1].values
     else:
-        inData = rawData
+        if not(os.path.isfile('enc')):
+            enc = OneHotEncoder()
+            enc.fit(rawData)
+            pickle.dump(enc, open("enc", "wb"))
+        else:
+            enc = pickle.load(open("enc","rb"))
+        inData = pd.DataFrame(enc.transform(rawData).toarray())
         outData = pd.DataFrame([0]*rawData.shape[0]).T.values[0]
-    return (inData,outData)
+
+    return inData, outData
 
 def convert_CsvToData_ratio(name, ratio = 0.5, size = 10000):
-    #ratio is the ratio of positif over the total
-    data = convert_CsvToData(name, False)[0]
-    data = data.sort_values(by = 'click')
-    n_d = data.shape[0]
-    data.index = [i for i in range(n_d)]
-    i_cut = data['click'].idxmax()
-    X = pd.DataFrame(data[0:1])
-    r = (1.0,0.0)
+    """Give a sample of data with a ratio of positive/(positive+negative).
+        To construct a new set of data, some of the previous can be ignored or duplicated.
+
+    Parameters
+    ----------
+    name: Name or relative path of the file with the extension.
+
+    ratio: Ratio of positive Target over the total amount of Target.
+
+    size: Size of the generated data.
+
+    Returns
+    -------
+    inData, outData: tuple containing a DataFrame of Inputs of the datafile and
+        an array of Target.
+    """
+    #Extract data from csv file, and put Nan value to 0
+    rawData = pd.read_csv(name, delimiter = ',', dtype={"dclkVerticals": object})
+    #del rawData['ip']
+    rawData.drop('ip', axis=1, inplace=True)
+    rawData = rawData.fillna(0)
+
+    #Encoder
+    if not(os.path.isfile('enc')):
+        enc = OneHotEncoder(handle_unknown='ignore')
+        enc.fit(rawData.iloc[:,:-1])
+        pickle.dump(enc, open("enc", "wb"))
+    else:
+        enc = pickle.load(open("enc","rb"))
+        
+    #Rearranging index after sorting and get the index of the first 1.
+    n_d = rawData.shape[0]
+    rawData = rawData.sort_values(by = 'click')
+    rawData.index = [i for i in range(n_d)]
+    i_cut = rawData['click'].idxmax()
+    data = pd.DataFrame(rawData[:1])
+    p, n = 0.0, 1.0
+    
     for i in range(size):
-        if r[1]/(r[1]+r[0]) < ratio:
-            i_rd = rd.randint(i_cut,n_d-1)
-            r = (r[0],r[1]+1)
+        if p/(p+n) < ratio:
+            i_rd = rd.randint(i_cut,n_d - 1)
+            p = p+1
         else:
-            i_rd = rd.randint(0,i_cut-1)
-            r = (r[0]+1,r[1])
-        X = X.append(data[i_rd:i_rd+1], ignore_index = True)
-        #print i_rd, data[i_rd:(i_rd+1)]
-    X = shuffle(X)
-    return (X.iloc[:,:-1],X.iloc[:,-1].values)
+            i_rd = rd.randint(0,i_cut - 1)
+            n = n+1
+        data = data.append(rawData[i_rd:i_rd+1], ignore_index = True)
+    data = shuffle(data)
+        
+    #Encode data
+    inData = pd.DataFrame(enc.transform(data.iloc[:,:-1]).toarray())
+    outData = data.iloc[:,-1].values
+    return inData, outData
         
 def convert_DataToCsv(data, name):
+    """Save the DataFrame with the associated name."""
     pd.DataFrame(data).to_csv(name, index=False)
-    
-def convert_string(string):
-    """Convert string into float by using Unicode decimal"""
-    if type(string) != str:
-        return float(string)
-    
-    value = 0
-    for c in string:
-        value = value + ord(c)
-    return float(value)
-
-
-#Examples
-##(X,y) = convert_CsvToData('TraData.csv')
-##(Xt, yt) = convert_CsvToData('input.csv',TrainingData = False)
-##convert_DataToCsv(yt,'output.csv')
