59e993749a29723b903f45f88bb1aac24f83b528
YannLocatelli <45776332+YannLocatelli@users.noreply.github.com>
2018-12-14 04:09:58 +0800

    Update Neural network.py
    Small changes

--- Neural network.py
+++ Neural network.py
@@ -14,7 +14,7 @@ def Results(brain, y_true, y_pred):
     rec = recall_score(y_true, y_pred)
     F = 2 * prec * rec / (prec + rec)
     print "Accuracy:",acc,"\nPrecision:",prec,"\nRecall:",rec,"\nF-Measure:",F,"\n"
-    
+    return F
 
 #X, y = convert_CsvToData('TraData.csv')
 X, y = convert_CsvToData_ratio('TraData.csv')
@@ -29,10 +29,9 @@ model = nn.MLPClassifier(hidden_layer_sizes = n, activation = 'logistic',
 #k-fold model
 for i in range(1000):
     k = rd.randint(5,20)
-    k_size = float(X.shape[0])/k
-    for j in range(k - 1):
-        sampleBot = int(k_size * j)
-        sampleTop = int(k_size * (j+1))
+    k_step = X.shape[0]/k
+    for j in range(0, (k-1)*k_step, k_step):
+        sampleBot, sampleTop = j, j + k_step
         model.fit(X[sampleBot:sampleTop],y[sampleBot:sampleTop])
     print i+1,"."
     Results(model,y[sampleTop:],model.predict(X[sampleTop:]))
