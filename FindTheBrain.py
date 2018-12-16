bf1acddffd9c2ccea7a3c01c4b34749269467b1e
YannLocatelli <45776332+YannLocatelli@users.noreply.github.com>
2018-12-16 23:54:10 +0800

    Update FindTheBrain.py
    Architecture changed: instead of trying combination, change the ratio of 1.
    The ratio evolve with a function that changed in a logarithmic way between 0 and 0.6

--- FindTheBrain.py
+++ FindTheBrain.py
@@ -2,33 +2,34 @@ from NeuralNetworkModel import *
 from ReadCSV import *
 import pandas as pd
 import time
+import random
+from math import log10
 
-d = 500 #data_size
-n = 100 #n_layers
-ratio_p = [0.12 * i for i in range(5,1,-1)] #ratio_p
-k_samples = [5,7,10,11,14,16,20] #k_samples
-a = 'tanh' #activation
-s = 'lbfgs' #solver
-se = None #seed
+d, n = 1000, 100 #data_size, n_layers
+#k_samples = [5,7,10,11,14,16,20]
+a, s, se = 'tanh', 'lbfgs', None #activation, solver, seed
 
 df = pd.DataFrame()
-n_combination = len(data_size) * len(n_layers) * len(ratio_p) * len(k_samples) * len(activation) *  len(solver) * len(seed)
 i = 0
-iStart = 0
-name = "Results.csv"
+name = "Results_100lays.csv"
 
-for r in ratio_p:
-    for k in k_samples:
-        i = i+1
-        if i < iStart:
-            continue
-        print i,"/",n_combination,":",d,n,r,k,a,s,se
-        stTime = time.time()
-        F_values = NN_Model(d,n,r,k,a,s,se)
-        fnTime = time.time()-stTime
-        df = df.append([[d,n,r,k,a,s,se,F_values[0],F_values[1],fnTime]],True)
-        convert_DataToCsv(df,name)
+ratio_p = 0.5 #ratio_p
+F_measure_Goal = 0.6
+ratio_num = log10(F_measure_Goal)
+ratio_denom = F_measure_Goal + log10(1.0 + 1.0/F_measure_Goal)
 
+while(ratio_p > 0.01):
+    k = random.randint(4,20)
+    i = i+1
+    print i,":",n,ratio_p,k
+    stTime = time.time()
+    F_values = NN_Model(d,n,ratio_p,k,a,s,se)
+    fnTime = time.time()-stTime
+    print "F_measure = %.2f\n" % F_values[0]
+    df = df.append([[d,n,ratio_p,k,a,s,se,F_values[0],F_values[1],fnTime]],True)
+    convert_DataToCsv(df,name)
+
+    ratio_p = F_measure_Goal * (ratio_p + log10(1 + F_measure_Goal - F_values[0]) - ratio_num)/ratio_denom
                             
 df.columns = ['Data Size', 'Number of layers', 'Ratio of 1 over all',
               'Number of k-samples', 'Type of activation', 'Type of solver',
