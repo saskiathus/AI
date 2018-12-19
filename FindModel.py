b4184e624ff3efcfe270edc8408691df39bf5fca
YannLocatelli <45776332+YannLocatelli@users.noreply.github.com>
2018-12-19 18:55:44 +0800

    Update FindModel.py
    Remove MLP which take too much time to fit.
    Adapt data size for testing (called X_train)
    Initial parameter were adapted too
    estimator removed in F_15
    ratio is now between 0 and 0.01
    
    Some bugs corrected

--- FindModel.py
+++ FindModel.py
@@ -26,7 +26,7 @@ F_15 = []
 
 
 ## Methods
-names = ["DT_5e","DT_5","DT_e","DT","ADC_Tla","ADC_a","MLP","ENS"]
+names = ["DT_5e","DT_5","DT_e","DT","ADC_Tla","ADC_a","ENS"]
 
 def generate_classifiers():
     clf = []
@@ -36,8 +36,6 @@ def generate_classifiers():
     clf = clf + [DecisionTreeClassifier()]
     clf = clf + [AdaBoostClassifier(DecisionTreeClassifier(max_depth=5,criterion="entropy"),learning_rate = 1.5,algorithm="SAMME")]
     clf = clf + [AdaBoostClassifier(algorithm="SAMME")]
-    clf = clf + [MLPClassifier(hidden_layer_sizes = (500,450,400),activation = 'tanh', tol = 1e-7, warm_start = True,
-                               alpha = 2e-5, solver = 'adam', max_iter = 700, random_state = None)]
 
     mod = []
     for name, clf in zip(names[:-1], clf):
@@ -46,8 +44,8 @@ def generate_classifiers():
     return mod
 
 ## Data
-X_train, y_train = convert_CsvToData('TraData.csv', index_end = 300000) #Ratio = 0.0005.
-X_trainb, y_trainb = convert_CsvToData('TraData.csv', index_start = 600000, index_end = 900000) #Ratio = 0.0005.
+X_train, y_train = convert_CsvToData('TraData.csv', index_end = 400000) #Ratio = 0.0005.
+X_trainb, y_trainb = convert_CsvToData('TraData.csv', index_start = 500000, index_end = 900000) #Ratio = 0.0005.
 X_out,y_out = convert_CsvToData('input.csv', TrainingData = False)
 print "Data extraction completed"
 
@@ -58,8 +56,8 @@ def generate_output(model,name = "output.csv",name_model = "model"):
 
 
 ## Parameters
-training_size = 3000
-ratio = random.random()*5e-3
+training_size = 5000
+ratio = 0.05
 len_F15_max = 15
 
 
@@ -76,7 +74,7 @@ while Fmax < 0.9:
 
         # Prediction
         print "Prediction..."
-        Fm = Results(y_train,clf.predict(X_train))
+        Fm = Results(y_train,clf.predict(X_train), False)
 
         # Display Results
         print "Size = %s, Ratio = %.6f\nFm = %.5f and Fsup = %.5f\n" % (training_size, ratio, Fm, Fsup)
@@ -87,8 +85,8 @@ while Fmax < 0.9:
             if Fm > Fmax:
                 Fmax = Fm
                 generate_output(clf)
-            if Fm > 0.15:
-                F_15 = F_15 + [(name,clf)]
+        if Fm > 0.15:
+            F_15 = F_15 + [(name,clf)]                
         Fmoy = Fmoy + Fm
 
     # Changing the threshold of F-measurement. Good Fm on large dataset are more intersting.    
@@ -98,29 +96,28 @@ while Fmax < 0.9:
     # Testing the best results
     print "\nLength of F_15:",len(F_15)
     if len(F_15) > len_F15_max:
-        print "Starting process to determine best method!"
-        results = []
+        print "Starting process to determine best method!"        
+        results, names_methods = [], ""
         for name, clf in F_15:
-            results = results.append(Results(y_trainb,clf.predict(X_trainb),False))
+            results = results + [Results(y_trainb,clf.predict(X_trainb),False)]
+            names_methods = names_methods + " " + name
+        print "List of methods: " + names_methods
         F_15 = [x for _,x in sorted(zip(results,F_15),reverse = True)]
         F_15 = F_15[:5]
-        best_Fm = Results(y_trainb,F_15[0][1].predict(X_trainb),False)
+        print "Best model:
+        best_Fm = Results(y_trainb,F_15[0][1].predict(X_trainb))
         saveName = "_%s_%.4f.csv" % (F_15[0][0],best_Fm)
         generate_output(F_15[0][1],"output"+saveName,"model"+saveName)
         print "Best model %s save with Fm = %.4f" % (F_15[0][0],best_Fm)
 
-        est = []
-        for name, clf in F_15:
-            est = est + [(name,clf)]
-        estimator = VotingClassifier(estimators=est)
-        print "Comparison with estimator: Fm = ", Results(y_trainb,estimator.predict(X_trainb), False)
+##        estimator = VotingClassifier(estimators=F_15)
+##        res = Results(y_trainb,estimator.predict(X_trainb), False)
+##        print "Comparison with estimator: Fm =",res
 
     # Preparing next step
     print "Next loop in Preparation...\n\n\n"
     training_size = training_size + 200
-    ratio = random.random()*5e-3
-    while ratio < 5e-4:
-        ratio = random.random()*5e-3
+    ratio = random.random()*1e-2
 
 
 ####### Classifiers tested #######
