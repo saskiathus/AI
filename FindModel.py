03b5007b96a13ebc712a710c3bb28ba50e38ef33
YannLocatelli <45776332+YannLocatelli@users.noreply.github.com>
2018-12-20 00:41:45 +0800

    Update FindModel.py
    Add RandomForstClassifier and over/under-sampling tools.
    
    More classifiers added
    max_depth change to 2 and 4
    Ratio removed
    F_15 standardized

--- FindModel.py
+++ FindModel.py
@@ -1,11 +1,18 @@
+# Documentation:    https://www.kaggle.com/rafjaa/resampling-strategies-for-imbalanced-datasets
+#                   https://beckernick.github.io/oversampling-modeling/
+
 from ReadCSV import *
 import pickle
 import random
 
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import VotingClassifier
-from sklearn.neural_network import MLPClassifier
+
+from imblearn.over_sampling import SMOTE
+from imblearn.under_sampling import TomekLinks
+from imblearn.combine import SMOTETomek
 
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
@@ -26,15 +33,26 @@ F_15 = []
 
 
 ## Methods
-names = ["DT_5e","DT_5","DT_e","DT","ADC_Tla","ADC_a","ENS"]
+names = ["DT_5e","DT_5","DT_2e","DT_2","DT_2eSqrt","DT_2Sqrt","DT_2eLog","DT_2Log","DT_e","DT","RFC_2e","RFC_4e","RFC_2","RFC_4","ADC_T4la","ADC_T2la","ADC_a","ENS"]
 
 def generate_classifiers():
     clf = []
-    clf = clf + [DecisionTreeClassifier(max_depth=5,criterion="entropy")]
-    clf = clf + [DecisionTreeClassifier(max_depth=5)]
+    clf = clf + [DecisionTreeClassifier(max_depth=4,criterion="entropy")]
+    clf = clf + [DecisionTreeClassifier(max_depth=4)]
+    clf = clf + [DecisionTreeClassifier(max_depth=2,criterion="entropy")]
+    clf = clf + [DecisionTreeClassifier(max_depth=2)]
+    clf = clf + [DecisionTreeClassifier(max_depth=2,criterion="entropy",max_features="sqrt")]
+    clf = clf + [DecisionTreeClassifier(max_depth=2,max_features="sqrt")]
+    clf = clf + [DecisionTreeClassifier(max_depth=2,criterion="entropy",max_features="log2")]
+    clf = clf + [DecisionTreeClassifier(max_depth=2,max_features="log2")]
     clf = clf + [DecisionTreeClassifier(criterion="entropy")]
     clf = clf + [DecisionTreeClassifier()]
-    clf = clf + [AdaBoostClassifier(DecisionTreeClassifier(max_depth=5,criterion="entropy"),learning_rate = 1.5,algorithm="SAMME")]
+    clf = clf + [RandomForestClassifier(n_estimators = 10, criterion = "entropy", max_depth = 2, n_jobs = 3)]
+    clf = clf + [RandomForestClassifier(n_estimators = 10, criterion = "entropy", max_depth = 4, n_jobs = 3)]
+    clf = clf + [RandomForestClassifier(n_estimators = 10, max_depth = 2, n_jobs = 3)]
+    clf = clf + [RandomForestClassifier(n_estimators = 10, max_depth = 4, n_jobs = 3)]
+    clf = clf + [AdaBoostClassifier(DecisionTreeClassifier(max_depth=4,criterion="entropy"),learning_rate = 1.5,algorithm="SAMME")]
+    clf = clf + [AdaBoostClassifier(DecisionTreeClassifier(max_depth=2,criterion="entropy"),learning_rate = 1.5,algorithm="SAMME")]
     clf = clf + [AdaBoostClassifier(algorithm="SAMME")]
 
     mod = []
@@ -44,9 +62,14 @@ def generate_classifiers():
     return mod
 
 ## Data
-X_train, y_train = convert_CsvToData('TraData.csv', index_end = 400000) #Ratio = 0.0005.
-X_trainb, y_trainb = convert_CsvToData('TraData.csv', index_start = 500000, index_end = 900000) #Ratio = 0.0005.
+X_train, y_train = convert_CsvToData('TraData.csv', index_end = 400000) #Ratio = 0.0005
+X_trainb, y_trainb = convert_CsvToData('TraData.csv', index_start = 500000, index_end = 900000) #Ratio = 0.0005
 X_out,y_out = convert_CsvToData('input.csv', TrainingData = False)
+
+
+smote = SMOTE(sampling_strategy = minority, k_neighbors = 3, n_jobs = 3)
+tomek = TomekLinks(sampling_strategy = 'auto' ,n_jobs =3)
+smt = SMOTETomek(smote = smote, tomek = tomek)
 print "Data extraction completed"
 
 def generate_output(model,name = "output.csv",name_model = "model"):
@@ -56,14 +79,15 @@ def generate_output(model,name = "output.csv",name_model = "model"):
 
 
 ## Parameters
-training_size = 5000
-ratio = 0.05
+training_size = 1000
 len_F15_max = 15
+F15_threshold = 0.10
 
 
 while Fmax < 0.9:
     # Data
-    X, y = convert_CsvToData_ratio('TraData.csv', ratio = ratio, size = training_size)
+    X, y = convert_CsvToData('TraData.csv',index_end = training_size, Sort = True)
+    X, y = smt.fit_resample(X,y)
     print "Data extraction completed"
 
     classifiers = generate_classifiers()
@@ -77,7 +101,7 @@ while Fmax < 0.9:
         Fm = Results(y_train,clf.predict(X_train), False)
 
         # Display Results
-        print "Size = %s, Ratio = %.6f\nFm = %.5f and Fsup = %.5f\n" % (training_size, ratio, Fm, Fsup)
+        print "Size = %s // Fm = %.5f // Fsup = %.5f\n" % (training_size, Fm, Fsup)
 
         # Comparing Results
         if Fm > Fsup:
@@ -85,7 +109,7 @@ while Fmax < 0.9:
             if Fm > Fmax:
                 Fmax = Fm
                 generate_output(clf)
-        if Fm > 0.15:
+        if Fm > F15_threshold:
             F_15 = F_15 + [(name,clf)]                
         Fmoy = Fmoy + Fm
 
@@ -104,10 +128,11 @@ while Fmax < 0.9:
         print "List of methods: " + names_methods
         F_15 = [x for _,x in sorted(zip(results,F_15),reverse = True)]
         F_15 = F_15[:5]
+        F15_threshold = Results(y_trainb,F_15[4][1].predict(X_trainb))
         print "Best model:
         best_Fm = Results(y_trainb,F_15[0][1].predict(X_trainb))
-        saveName = "_%s_%.4f.csv" % (F_15[0][0],best_Fm)
-        generate_output(F_15[0][1],"output"+saveName,"model"+saveName)
+        saveName = "_%s_%.4f" % (F_15[0][0],best_Fm)
+        generate_output(F_15[0][1],"output"+saveName+".csv","model"+saveName+".model")
         print "Best model %s save with Fm = %.4f" % (F_15[0][0],best_Fm)
 
 ##        estimator = VotingClassifier(estimators=F_15)
@@ -117,7 +142,6 @@ while Fmax < 0.9:
     # Preparing next step
     print "Next loop in Preparation...\n\n\n"
     training_size = training_size + 200
-    ratio = random.random()*1e-2
 
 
 ####### Classifiers tested #######
